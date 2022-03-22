240ae7b74f1152975b392eb42b9f9dd0a700a003
Daniel Armbruster <danielgundam00@gmail.com>
2022-03-22 15:32:49 -0600

    commit tree 3cdb4038061f959f766f3ea8306c0bb2b7a1a3bb

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,35 @@
 import React from "react";
 import { movies } from "../data";
 
+
+
+
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+  const movie = movies.map((movie) =>{
+  
+  
+    return(
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <p>{movie.time}</p> 
+      <ul>
+      {movie.genres.map((genre) => {
+        return (
+          <li key={genre}>{genre}</li>
+        )
+      })}</ul></div>
+  )});
+  
+  
+  
+  return (<div>
+    <h1>Movies Page</h1>
+    <>
+      {movie}
+    </>
+  </div>);
 }
 
 export default Movies;
